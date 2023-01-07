e0299598ec0bef2cf871e8539a2f5d8f578986e5
halitSen <halit.sen@vngrs.com>
2023-01-07 17:41:39 +0300

    note list design

--- app/src/main/java/com/example/postpone/home/NoteScreen.kt
+++ app/src/main/java/com/example/postpone/home/NoteScreen.kt
@@ -1,78 +1,107 @@
-package com.example.postpone.screen
+package com.example.postpone.home
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.postpone.model.Note
+import com.example.postpone.utils.getDateFromTimeStamp
 
 @Composable
 fun NoteScreen(notes: List<Note>, onNoteClicked: (Note) -> Unit) {
-    LazyColumn(modifier = Modifier.background(color = Color.White)) {
+    LazyColumn(
+        modifier = Modifier
+            .background(color = MaterialTheme.colors.background)
+            .padding(top = 6.dp)
+    ) {
         items(notes) { item ->
             NoteRow(note = item, onNoteClicked = onNoteClicked)
         }
     }
 }
 
+@Preview
 @Composable
 fun NoteRow(
     modifier: Modifier = Modifier,
-    note: Note,
-    onNoteClicked: (Note) -> Unit
+    note: Note = Note(description = "Note Description", noteTitle = "Note Title"),
+    onNoteClicked: (Note) -> Unit = {}
 ) {
+    var interactionSource = remember { MutableInteractionSource() }
 
     Surface(
         modifier = modifier
-            .padding(top = 6.dp, bottom = 6.dp, start = 6.dp, end = 6.dp)
-            .clip(
-                shape = RoundedCornerShape(
-                    topEnd = 16.dp,
-                    bottomStart = 16.dp,
-                    bottomEnd = 16.dp
-                )
-            )
-            .fillMaxWidth(),
-        color = Color(0xFF1F2544)
+            .background(MaterialTheme.colors.background)
+            .padding(bottom = 6.dp, start = 6.dp, end = 6.dp)
+            .fillMaxWidth()
     ) {
 
         Column(
-            modifier = modifier
-                .padding(horizontal = 12.dp, vertical = 6.dp)
-                .clickable {
-                    onNoteClicked.invoke(note)
-                }, horizontalAlignment = Alignment.Start
+            modifier = Modifier
+                .background(MaterialTheme.colors.background)
+                .padding(horizontal = 12.dp), verticalArrangement = Arrangement.Center
         ) {
-            Text(
-                text = note.noteTitle,
-                style = MaterialTheme.typography.subtitle2,
-                color = Color.White
-            )
-            Text(
-                text = note.description,
-                style = MaterialTheme.typography.subtitle1,
-                color = Color.White
-            )
-            Text(
-                text = note.description,
-                style = MaterialTheme.typography.caption,
-                color = Color.White
+            Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier
+                .clickable(
+                    interactionSource = interactionSource,
+                    indication = null
+                ) {
+                    onNoteClicked.invoke(note)
+                }) {
+                Column(
+                    modifier = modifier.weight(1f, true)
+                ) {
+                    Text(
+                        text = getDateFromTimeStamp(note.noteLastEdit),
+                        style = TextStyle(
+                            fontSize = 12.sp,
+                            color = MaterialTheme.colors.secondary.copy(0.7f)
+                        )
+                    )
+                    Text(
+                        text = note.description,
+                        style = TextStyle(
+                            fontSize = 16.sp,
+                            color = MaterialTheme.colors.secondary
+                        ),
+                        modifier = modifier.padding(top = 6.dp, bottom = 6.dp)
+                    )
+                }
+                Icon(
+                    Icons.Outlined.Delete,
+                    contentDescription = "",
+                    tint = MaterialTheme.colors.secondary,
+                    modifier = Modifier.padding(12.dp)
+                )
+            }
+            Divider(
+                Modifier
+                    .height(0.5.dp)
+                    .background(MaterialTheme.colors.secondary)
             )
         }
 
     }
-
 }
